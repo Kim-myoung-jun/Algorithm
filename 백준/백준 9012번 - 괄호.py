8441c1a01a52b7756698386ae7ec800854216f0f
MyoungJun Kim / 김명준 <66722728+kmjun0321@users.noreply.github.com>
2022-10-11 15:08:24 +0900

    Update 백준 9012번 - 괄호.py

--- 백준/백준 9012번 - 괄호.py
+++ 백준/백준 9012번 - 괄호.py
@@ -21,3 +21,22 @@ for _ in range(n):
   else:
     print(flag)
   
+  '''
+from sys import stdin
+
+n = int(input())
+for _ in range(n):
+    str_ = stdin.readline().strip()
+    stack = 0
+    for chr_ in str_:
+        if chr_ == '(':
+            stack += 1
+        else:
+            stack -= 1
+            if stack < 0:
+                break
+    if stack == 0:
+        print('YES')
+    else:
+        print('NO')
+'''
