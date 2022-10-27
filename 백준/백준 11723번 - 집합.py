b631a59299e7b0a8a125e8c8f2c7f15873e18bbb
MyoungJun Kim / 김명준 <66722728+kmjun0321@users.noreply.github.com>
2022-10-27 09:44:23 +0900

    Create 백준 11723번 - 집합.py

---
+++ 백준/백준 11723번 - 집합.py
@@ -0,0 +1,27 @@
+# https://www.acmicpc.net/problem/11723
+import sys
+m = int(sys.stdin.readline())
+s = 0b0
+for _ in range(m):
+    o = sys.stdin.readline().rstrip().split()
+    if o[0] == "all":
+        s = 0b111111111111111111111
+    elif o[0] == "empty":
+        s = 0b0
+    else:
+        order = o[0]
+        x = int(o[1])
+        if order == "add":
+            s = s | (0b1 << x)
+        elif order == "remove":
+            s = s & ~(0b1 << x)
+        elif order == "check":
+            if s & (0b1 << x):
+                print(1)
+            else:
+                print(0)
+        #elif order == "toggle":
+        else:
+            s = s ^ (0b1 << x)
+            
+  # ref - https://coarmok.tistory.com/entry/%ED%8C%8C%EC%9D%B4%EC%8D%ACpython-%EB%B0%B1%EC%A4%80-11723%EB%B2%88-%EB%B9%84%ED%8A%B8%EB%A7%88%EC%8A%A4%ED%82%B9?category=910483
