fefd40103ce5cb7bc76b168ab30d229301f8ff86
MyoungJun Kim / 김명준 <66722728+kmjun0321@users.noreply.github.com>
2022-11-25 11:28:04 +0900

    Create 백준 1074번 - Z.py

---
+++ 백준/백준 1074번 - Z.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n, r, c = map(int, input().split())
+idx = 0
+while n != 0:
+  n -= 1
+  if r < 2**n and c < 2**n:
+    idx += (2**n) * (2**n) * 0
+  elif r < 2**n and c >= 2**n:
+    idx += (2**n) * (2**n) * 1
+    c -= 2**n
+  elif r >= 2**n and c < 2**n:
+    idx += (2**n) * (2**n) * 2
+    r -= 2**n
+  else:
+    idx += (2**n) * (2**n) * 3
+    r -= 2**n
+    c -= 2**n
+print(idx)
