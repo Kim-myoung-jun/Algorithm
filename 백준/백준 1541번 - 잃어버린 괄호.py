aca757ff8804ca72ed1363c9207feef131b95666
MyoungJun Kim / 김명준 <66722728+kmjun0321@users.noreply.github.com>
2022-12-06 19:35:13 +0900

    Create 백준 1541번 - 잃어버린 괄호.py

---
+++ 백준/백준 1541번 - 잃어버린 괄호.py
@@ -0,0 +1,38 @@
+s = input()
+num = []
+oper = []
+input_num = ""
+for i in range(len(s)):
+  if ord(s[i]) >= ord('0') and ord(s[i]) <= ord('9'):
+    input_num += s[i]
+  else:
+    num.append(int(input_num))
+    input_num = ""
+    oper.append(s[i])
+num.append(int(input_num))
+
+#print(num)
+#print(oper)
+
+for i in range(len(oper)):
+  if oper[i] == '+':
+    #print(type(num[i]))
+    if type(num[i]) is int:
+      num[i] = num[i]+num[i+1]
+      num[i+1] = [num[i], i]
+      oper[i] = 'x'
+    else:
+      num[num[i][1]] = num[i][0] + num[i+1]
+      num[i+1] = [num[num[i][1]], num[i][1]]
+      oper[i] = 'x'
+sum = 0
+check = False
+for i in range(len(num)):
+  if type(num[i]) is int:
+    if check == False:
+      sum = num[i]
+      check = True
+    else:
+      sum -= num[i]
+
+print(sum)
