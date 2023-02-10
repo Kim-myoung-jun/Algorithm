f9d12e6b56d362b1d9f5909c5619e4f935ffbdf5
MyoungJun Kim / 김명준 <66722728+kmjun0321@users.noreply.github.com>
2023-02-10 13:26:57 +0900

    Create 백준 15652번 - N과 M(4).py

---
+++ 백준/백준 15652번 - N과 M(4).py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+stack = []
+def dfs(start):
+  if len(stack) == m:
+    for i in range(m):
+      print(stack[i], end=' ')
+    print()
+    return
+  for i in range(start, n+1):
+    stack.append(i)
+    dfs(i)
+    stack.pop()
+dfs(1)
